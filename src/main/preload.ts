1a01cf3a59a1c8e21009bcd96f753e37029d355f
blanca <978059629@qq.com>
2022-05-15 00:46:22 +0800

    reject and reconnection bug 1

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -3,8 +3,69 @@ const {contextBridge, ipcRenderer, desktopCapturer} = require('electron')
 let pc: RTCPeerConnection = new RTCPeerConnection()
 let remoteSenders: RTCRtpSender[] = []
 let candidates: any[] = []
-let mediaScreen: MediaStream
+let mediaScreen: MediaStream | null
 let isInitGetMediaScreen: boolean = true
+contextBridge.exposeInMainWorld('electronAPI', {
+  getLocalChannel: async function () {
+    let result = await ipcRenderer.invoke('getLocalChannel')
+    return result
+  },
+  addWhoCallListener: function (callback) {
+    ipcRenderer.on('whoCall', (e, channel) => {
+      callback(channel)
+    })
+  },
+  addCloseConnectionListener: function (callback) {
+    ipcRenderer.on('closeConnect', async () => {
+      for (let i = 0; i < remoteSenders.length; i++) {
+        pc.removeTrack(remoteSenders[i])
+      }
+      initEnv()
+      callback()
+    })
+  },
+  callerToCall: function (remoteChannel, callerToCallResultCallback, calleeAcceptCall, calleeRejectCall) {
+    ipcRenderer.send('callerToCall', remoteChannel)
+    ipcRenderer.once('callerToCallResult', (e, result) => {
+      callerToCallResultCallback(result)
+      if (result.code === 0) {
+        ipcRenderer.once('calleeAcceptCall', (e, remoteChannel) => {
+          callerSendOffer()
+          calleeAcceptCall(remoteChannel)
+          removerIpcRendererListener('calleeRejectCall')
+        })
+        ipcRenderer.once('calleeRejectCall', (e, remoteChannel) => {
+          calleeRejectCall(remoteChannel)
+          removerIpcRendererListener('calleeAcceptCall', 'calleeSendAnswer', 'calleeSendCandidate')
+        })
+        ipcRenderer.once('calleeSendAnswer', callerSetAnswer)
+        ipcRenderer.on('calleeSendCandidate', addIceCandidate)
+      }
+    })
+  },
+
+  acceptCall: async function (remoteChannel, calleeAcceptCallResultCallback) {
+    ipcRenderer.send('calleeAcceptCall', remoteChannel)
+    ipcRenderer.once('calleeAcceptCallResult', (e, result) => {
+      calleeAcceptCallResultCallback(result)
+      if (result.code === 0) {
+        ipcRenderer.once('callerSendOffer', calleeSetOfferAndSendAnswer)
+        ipcRenderer.on('callerSendCandidate', addIceCandidate)
+      }
+    })
+  },
+  rejectCall: async function (remoteChannel) {
+    ipcRenderer.send('calleeRejectCall', remoteChannel)
+  },
+  closeConnect: async function (remoteChannel) {
+    for (let i = 0; i < remoteSenders.length; i++) {
+      pc.removeTrack(remoteSenders[i])
+    }
+    initEnv()
+
+    ipcRenderer.send('closeConnect', remoteChannel)
+  },
+})
 async function addIceCandidate(e, candidate) {
   try {
     let candi = JSON.parse(candidate)
@@ -24,14 +85,48 @@ async function addIceCandidate(e, candidate) {
     console.log('当前还未添加远程端信息，将candidate放入数组')
   }
 }
-const getMediaScreen = async () => {
+function stopVideo() {
+  getMediaScreen()
+    .then(stream => {
+      stream.getTracks().forEach(track => {
+        track.stop()
+        console.log(track.kind, '关闭', '状态：', track.readyState)
+      })
+    })
+    .finally(() => {
+      isInitGetMediaScreen = true
+    })
+}
+async function initEnv() {
+  pc = new RTCPeerConnection()
+  candidates = []
+  isInitGetMediaScreen = true
+  mediaScreen = null
+  const ipcChannels: string[] = [
+    'callerToCallResult',
+    'callerToCallResult',
+    'calleeAcceptCall',
+    'calleeRejectCall',
+    'calleeSendAnswer',
+    'calleeSendCandidate',
+    'calleeAcceptCallResult',
+    'callerSendOffer',
+    'callerSendCandidate',
+  ]
+  for (let i = 0; i < ipcChannels.length; i++) {
+    ipcRenderer.removeAllListeners(ipcChannels[i])
+  }
+  addTrackCallback()
+  stopVideo()
+}
+async function getMediaScreen() {
   if (!mediaScreen || isInitGetMediaScreen) {
     isInitGetMediaScreen = false
     mediaScreen = await navigator.mediaDevices.getUserMedia({audio: true, video: true})
   }
   return mediaScreen
 }
-const addVideoSrcObjec = (remoteStream, localStream) => {
+function addVideoSrcObjec(remoteStream, localStream) {
   let remoteVideo = document.getElementById('remoteVideo') as HTMLVideoElement
   let localvideo = document.getElementById('localVideo') as HTMLVideoElement
   remoteVideo.srcObject = remoteStream
@@ -45,19 +140,8 @@ const addVideoSrcObjec = (remoteStream, localStream) => {
     localvideo.play()
   }
 }
-const stopVideo = () => {
-  getMediaScreen()
-    .then(stream => {
-      stream.getTracks().forEach(track => {
-        track.stop()
-        console.log(track.kind, '关闭', '状态：', track.readyState)
-      })
-    })
-    .finally(() => {
-      isInitGetMediaScreen = true
-    })
-}
-const addTrackCallback = async function () {
+
+async function addTrackCallback() {
   pc.ontrack = async ev => {
     console.log('ontrack：有媒体流进入')
     let stream = await getMediaScreen()
@@ -75,7 +159,7 @@ const addTrackCallback = async function () {
   }
 }
 addTrackCallback()
-const callerSendOffer = async () => {
+async function callerSendOffer() {
   console.log('呼叫人 send offer')
   pc.onicecandidate = function (e) {
     if (e.candidate !== null) {
@@ -96,7 +180,7 @@ const callerSendOffer = async () => {
   await pc.setLocalDescription(offer)
   ipcRenderer.send('forward', 'callerSendOffer', {type: pc.localDescription?.type, sdp: pc.localDescription?.sdp})
 }
-const calleeSetOfferAndSendAnswer = async (e, offer) => {
+async function calleeSetOfferAndSendAnswer(e, offer) {
   console.log('被呼叫人：设置offer并发送answer')
   console.log('收到的offer:' + typeof offer + ':' + offer)
   pc.onicecandidate = e => {
@@ -114,67 +198,14 @@ const calleeSetOfferAndSendAnswer = async (e, offer) => {
   console.log('被呼叫人创建的answer:', JSON.stringify(answer))
   ipcRenderer.send('forward', 'calleeSendAnswer', {type: pc.localDescription?.type, sdp: pc.localDescription?.sdp})
 }
-const callerSetAnswer = async (e, answer) => {
+async function callerSetAnswer(e, answer) {
   console.log('呼叫人：收到answer并设置,answer:' + typeof answer + ':' + answer)
   await pc.setRemoteDescription(answer) //出错
   console.log('呼叫人已经设置上远程描述：', pc.remoteDescription)
   addIceCandidate(null, null)
 }
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getLocalChannel: async function () {
-    let result = await ipcRenderer.invoke('getLocalChannel')
-    return result
-  },
-  callerToCall: function (remoteChannel, callerToCallResultCallback, calleeAcceptCall, calleeRejectCall) {
-    ipcRenderer.send('callerToCall', remoteChannel)
-    ipcRenderer.once('callerToCallResult', (e, result) => {
-      callerToCallResultCallback(result)
-      if (result.code === 0) {
-        ipcRenderer.once('calleeAcceptCall', (e, remoteChannel) => {
-          callerSendOffer()
-          calleeAcceptCall(remoteChannel)
-        })
-        ipcRenderer.once('calleeRejectCall', (e, remoteChannel) => {
-          calleeRejectCall(remoteChannel)
-        })
-        ipcRenderer.once('calleeSendAnswer', callerSetAnswer)
-        ipcRenderer.on('calleeSendCandidate', addIceCandidate)
-      }
-    })
-  },
-  addWhoCallListener: function (callback) {
-    ipcRenderer.on('whoCall', (e, channel) => {
-      callback(channel)
-    })
-  },
-  addCloseConnectionListener: function (callback) {
-    ipcRenderer.on('closeConnect', async () => {
-      for (let i = 0; i < remoteSenders.length; i++) {
-        pc.removeTrack(remoteSenders[i])
-      }
-      stopVideo()
-      callback()
-    })
-  },
-  acceptCall: async function (remoteChannel, calleeAcceptCallResultCallback) {
-    ipcRenderer.send('calleeAcceptCall', remoteChannel)
-    ipcRenderer.once('calleeAcceptCallResult', (e, result) => {
-      calleeAcceptCallResultCallback(result)
-      if (result.code === 0) {
-        ipcRenderer.once('callerSendOffer', calleeSetOfferAndSendAnswer)
-        ipcRenderer.on('callerSendCandidate', addIceCandidate)
-      }
-    })
-  },
-  rejectCall: async function (remoteChannel) {
-    ipcRenderer.send('calleeRejectCall', remoteChannel)
-  },
-  closeConnect: async function (remoteChannel) {
-    for (let i = 0; i < remoteSenders.length; i++) {
-      pc.removeTrack(remoteSenders[i])
-    }
-    stopVideo()
-    ipcRenderer.send('closeConnect', remoteChannel)
-  },
-})
+function removerIpcRendererListener(...channels) {
+  channels.forEach(channel => {
+    ipcRenderer.removeAllListeners(channel)
+  })
+}
